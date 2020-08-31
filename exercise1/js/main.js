da6c0ceddefb7059f157a4ea2f535d417100b935
surya-adipn <70506936+surya-adipn@users.noreply.github.com>
2020-08-31 15:08:06 +0700

    Update main.js

--- exercise1/js/main.js
+++ exercise1/js/main.js
@@ -3,6 +3,7 @@ function msg() {
     var pwd = document.getElementById("password").value;
     if (email.length == 0 || pwd.length == 0) {
         alert("Pastikan anda telah mengisi email dan password anda dengan benar.")
+        clearField();
     }
     else if (pwd.length < 4) {
         alert("Password minimal 4 karakter, mohon ulangi kembali.")
